355161dd0bd0c4b764ba5bf029f6c8216b01c3ca
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-07-18 07:57:36 -0300

    ajuste banco de dados

--- criar_banco.py
+++ criar_banco.py
@@ -1,6 +1,8 @@
 from pyterest import app, database
 from pyterest.models import Usuario, Foto
 
+
+
 with app.app_context():
     database.drop_all()
     database.create_all()
--- pyterest/__init__.py
+++ pyterest/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "cd4ca554a626be7a6e9aa6c057e3b140"
 app.config["UPLOAD_FOLDER"] = "static/fotos-posts"
 
