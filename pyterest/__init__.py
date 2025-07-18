d4a48daf111da4e40a995c8163b4868a882db179
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-07-18 07:14:41 -0300

    edição no inity
    indicando a pasta onde esta os html

--- pyterest/__init__.py
+++ pyterest/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
