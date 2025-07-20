4cd95b2f2ce2a9d57402ef3cb0113e4a16653c7a
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-07-20 04:51:35 -0300

    mudei oaminho do banco

--- pyterest/__init__.py
+++ pyterest/__init__.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://dados_pynterest_user:5mldGzWsLudXQc2xmS6cpov9OeaHzcOJ@dpg-d1t1bcjuibrs738pdvag-a.oregon-postgres.render.com/dados_pynterest"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "cd4ca554a626be7a6e9aa6c057e3b140"
